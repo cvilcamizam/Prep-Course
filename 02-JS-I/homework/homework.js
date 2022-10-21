4d6ac8ae1fef9a1528fd853faadbcc91834a51ab
cvilcamizam <cvilcamizam@gmail.com>
2022-10-20 20:18:41 -0500

    JSI test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -171,9 +171,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0){
+  if(numero === 0) {
+    return false;
+  }
+  else if(numero > 0) {
     return "Es positivo";
-  } else{
+  }
+  else {
     return "Es negativo";
   }
 }
@@ -189,7 +193,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  return nombre + apellido;
 }
 
 function obtenerSaludo(nombre) {
